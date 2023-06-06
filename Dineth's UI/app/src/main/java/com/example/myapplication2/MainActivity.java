c5d18979cc36c939b5fc392229fdccd66c100ef9
Dineth-Udugamasooriya <115542491+Dineth-Udugamasooriya@users.noreply.github.com>
2023-06-06 19:30:14 +0530

    update

--- Dineth's UI/app/src/main/java/com/example/myapplication2/MainActivity.java
+++ Dineth's UI/app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView btnGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnGoogle = findViewById(R.id.btnGoogle);
+
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,GoogleSignInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
