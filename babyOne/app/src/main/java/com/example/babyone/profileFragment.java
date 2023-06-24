299fc7a59b8e2e2679f8cceb020cb9b59253e9fd
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2023-06-24 18:06:49 +0530

    Add features to profile Fragment

--- babyOne/app/src/main/java/com/example/babyone/profileFragment.java
+++ babyOne/app/src/main/java/com/example/babyone/profileFragment.java
@@ -2,11 +2,25 @@ package com.example.babyone;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.Fragment;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +37,9 @@ public class profileFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private static final String TAG = "DatabaseActivity";
+    private FirebaseFirestore db;
 
     public profileFragment() {
         // Required empty public constructor
@@ -61,4 +78,43 @@ public class profileFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        textViewUserData = view.findViewById(R.id.text_view_user_data);
+        db = FirebaseFirestore.getInstance();
+
+        // Read data from "users" collection
+        readUsersCollection();
+    }
+
+    private void readUsersCollection() {
+        CollectionReference usersCollectionRef = db.collection("users");
+
+        usersCollectionRef.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot querySnapshot) {
+                        StringBuilder userData = new StringBuilder();
+
+                        for (DocumentSnapshot documentSnapshot : querySnapshot) {
+                            String babyName = documentSnapshot.getString("Baby Name");
+                            String birthDate = documentSnapshot.getString("Birth Date");
+
+                            userData.append("Baby Name: ").append(babyName).append("\n")
+                                    .append("Birth Date: ").append(birthDate).append("\n\n");
+                        }
+
+                        textViewUserData.setText(userData.toString());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Handle error
+                    }
+                });
+    }
+
 }
