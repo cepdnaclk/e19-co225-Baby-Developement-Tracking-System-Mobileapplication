c158ad43f3fcbf2033d9bc9d5e5eee401bb8ccf3
CColombus <115539695+CColombus@users.noreply.github.com>
2023-06-25 22:23:54 +0530

    Working Vaccines page version 1

--- babyOne/app/src/main/java/com/example/babyone/VaccineActivity.java
+++ babyOne/app/src/main/java/com/example/babyone/VaccineActivity.java
@@ -1,9 +1,12 @@
 package com.example.babyone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class VaccineActivity extends AppCompatActivity {
 
@@ -16,5 +19,23 @@ public class VaccineActivity extends AppCompatActivity {
         setContentView(R.layout.activity_vaccine);
 
         recviewVaccine = findViewById(R.id.recviewVaccine);
+
+        ArrayList<VaccineModel> vaccineList = new ArrayList<>();
+        vaccineList.add(new VaccineModel("Ebola","21/02/2015","given"));
+        vaccineList.add(new VaccineModel("Corona","21/02/2011","given"));
+        vaccineList.add(new VaccineModel("Dengue","21/02/2012","notgiven"));
+        vaccineList.add(new VaccineModel("Polio","21/02/2011","given"));
+        vaccineList.add(new VaccineModel("Sars","21/02/2010","given"));
+        vaccineList.add(new VaccineModel("I","21/02/2010","given"));
+        vaccineList.add(new VaccineModel("Don\'t","21/02/2010","given"));
+        vaccineList.add(new VaccineModel("Care","21/02/2010","given"));
+        vaccineList.add(new VaccineModel("Anymore","21/02/2010","given"));
+
+
+        VaccineRecViewAdapter adapter = new VaccineRecViewAdapter();
+        adapter.setVaccines(vaccineList);
+
+        recviewVaccine.setAdapter(adapter);
+        recviewVaccine.setLayoutManager(new LinearLayoutManager(this));
     }
 }
