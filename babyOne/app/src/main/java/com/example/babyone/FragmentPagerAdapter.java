4f85437d199d64a7661e59789ce6cebc671c9e82
CColombus <115539695+CColombus@users.noreply.github.com>
2023-06-25 15:30:09 +0530

    Main Landing ViewPager done

---
+++ babyOne/app/src/main/java/com/example/babyone/FragmentPagerAdapter.java
@@ -0,0 +1,28 @@
+package com.example.babyone;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.List;
+
+public class FragmentPagerAdapter extends FragmentStateAdapter {
+    private final List<Fragment> fragments;
+
+    public FragmentPagerAdapter(FragmentActivity fa, List<Fragment> fragments) {
+        super(fa);
+        this.fragments = fragments;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragments.size();
+    }
+}
