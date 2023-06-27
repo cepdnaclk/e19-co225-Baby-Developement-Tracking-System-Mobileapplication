58beb710159a3d7d93f4e8d1f0e7e2ae020ca8cf
CColombus <115539695+CColombus@users.noreply.github.com>
2023-06-27 09:15:04 +0530

    Removed ProfileActivity and replacing with MainLanding Marked un-translatable strings proper back button for vaccine update activity

--- babyOne/app/src/main/java/com/example/babyone/VaccineUpdate.java
+++ babyOne/app/src/main/java/com/example/babyone/VaccineUpdate.java
@@ -23,24 +23,7 @@ public class VaccineUpdate extends AppCompatActivity {
         btnback_2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Create an instance of the profileFragment
-                profileFragment fragment = new profileFragment();
-
-                // Get the FragmentManager
-                FragmentManager fragmentManager = getSupportFragmentManager();
-
-                // Begin the fragment transaction
-                FragmentTransaction transaction = fragmentManager.beginTransaction();
-
-                // Replace the current fragment with the profileFragment
-                transaction.replace(R.id.profileFragment, fragment);
-
-                // Add the transaction to the back stack (optional)
-                transaction.addToBackStack(null);
-
-                // Commit the transaction
-                transaction.commit();
-
+                finish();
             }
         });
     }
