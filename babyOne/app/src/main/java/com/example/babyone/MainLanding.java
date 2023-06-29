abda732ea2f347ed757b082e3564041d77a88e8d
CColombus <115539695+CColombus@users.noreply.github.com>
2023-06-29 19:43:16 +0530

    Something's strange with this commit

--- babyOne/app/src/main/java/com/example/babyone/MainLanding.java
+++ babyOne/app/src/main/java/com/example/babyone/MainLanding.java
@@ -1,21 +1,14 @@
 package com.example.babyone;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
-import android.view.View;
 
 import com.example.babyone.databinding.ActivityMainLandingBinding;
-import com.example.babyone.databinding.ActivityProfileBinding;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainLanding extends AppCompatActivity {
 
