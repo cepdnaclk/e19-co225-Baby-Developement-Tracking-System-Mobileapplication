e29e91953ec6c7f1ddcca90ff345913997a02c0c
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2023-07-02 20:07:39 +0530

    Update BabyVaccination.java

--- babyOne/app/src/main/java/com/example/babyone/BabyVaccination.java
+++ babyOne/app/src/main/java/com/example/babyone/BabyVaccination.java
@@ -8,6 +8,7 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.ParseException;
