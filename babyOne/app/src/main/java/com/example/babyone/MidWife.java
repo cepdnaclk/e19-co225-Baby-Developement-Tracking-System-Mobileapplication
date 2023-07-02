dde7d65809fd036c0f4631fbaf4bfcd75e98b0e3
Dineth-Udugamasooriya <115542491+Dineth-Udugamasooriya@users.noreply.github.com>
2023-07-02 23:42:09 +0530

    midwife update

--- babyOne/app/src/main/java/com/example/babyone/MidWife.java
+++ babyOne/app/src/main/java/com/example/babyone/MidWife.java
@@ -2,6 +2,8 @@ package com.example.babyone;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,11 +11,21 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.card.MaterialCardView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MidWife extends AppCompatActivity {
 
     private SearchView searchView;
-    private MaterialCardView card2;
+    /*private MaterialCardView card1;*/
+    private FirebaseFirestore db;
+
+    private RecyclerView recyclerView;
+
+    private BabyAdapterMidWife adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,36 +33,116 @@ public class MidWife extends AppCompatActivity {
         setContentView(R.layout.activity_mid_wife);
 
         searchView = findViewById(R.id.searchView2);
-        card2 = findViewById(R.id.card2);
+        /*card1 = findViewById(R.id.card1);*/
+        recyclerView = findViewById(R.id.babyRecyclerView2);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        db = FirebaseFirestore.getInstance();
+
+        // Specify the collection name
+        String collectionName = "guardians";
+
+        adapter = new BabyAdapterMidWife(new ArrayList<>(), this);
+        recyclerView.setAdapter(adapter);
 
         searchView.clearFocus();
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MidWife.this, "searching " + query, Toast.LENGTH_SHORT).show();
 
-                card2.setVisibility(View.VISIBLE);
-                Toast.makeText(MidWife.this, "searching", Toast.LENGTH_SHORT).show();
-                //retrieve data from the database and display relevent data in the card view
+                // Retrieve data from the database and update the adapter with relevant data
+                db.collection("guardians")
+                        .whereGreaterThanOrEqualTo("babyname", query)
+                        .whereLessThan("babyname", query + "z")
+                        .get()
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                List<Guardian> guardians = new ArrayList<>();
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    String babyName = document.getString("babyname");
+                                    String parentName = document.getString("parentname");
+                                    String email = document.getString("email");
+
+                                    Guardian guardian = new Guardian(babyName, parentName, email);
+                                    guardians.add(guardian);
+                                }
+
+                                adapter.updateData(guardians);
+                            }
+                        });
+
                 return false;
             }
-
 
             @Override
-            public boolean onQueryTextChange(String newText) {
+            public boolean onQueryTextChange(String query) {
+                Toast.makeText(MidWife.this, "searching " + query, Toast.LENGTH_SHORT).show();
+
+                // Retrieve data from the database and update the adapter with relevant data
+                db.collection("guardians")
+                        .whereGreaterThanOrEqualTo("babyname", query)
+                        .whereLessThan("babyname", query + "z")
+                        .get()
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                List<Guardian> guardians = new ArrayList<>();
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    String babyName = document.getString("babyname");
+                                    String parentName = document.getString("parentname");
+                                    String email = document.getString("email");
+
+                                    Guardian guardian = new Guardian(babyName, parentName, email);
+                                    guardians.add(guardian);
+                                }
+
+                                adapter.updateData(guardians);
+                            }
+                        });
+
                 return false;
             }
+
         });
 
-        card2.setOnClickListener(new View.OnClickListener() {
+        /*card1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Start the MainLanding activity
+                // Start the MainLandin activity
                 // Assuming you have a button or click event to navigate to MainLanding
-                Intent intent = new Intent(MidWife.this, MainLanding.class);
-                intent.putExtra("sourceFragment", "midwife");
+                Intent intent = new Intent(Doctor.this, MainLanding.class);
+                intent.putExtra("sourceFragment", "doctor");
                 startActivity(intent);
-
             }
-        });
+        });*/
+
+        // Query the guardians collection to retrieve all documents
+        db.collection(collectionName)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        List<Guardian> guardians = new ArrayList<>();
+
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            // Extract the guardian data from the document
+                            String babyName = document.getString("babyname");
+                            String parentName = document.getString("parentname");
+                            String email = document.getString("email");
+
+                            // Create a Guardian object
+                            Guardian guardian = new Guardian(babyName, parentName, email);
+                            guardians.add(guardian);
+                        }
+
+                        // Set up the RecyclerView adapter
+                        BabyAdapter adapter = new BabyAdapter(guardians, this);
+                        recyclerView.setAdapter(adapter);
+                    } else {
+                        // Handle error
+                    }
+                });
+
+
     }
 }
