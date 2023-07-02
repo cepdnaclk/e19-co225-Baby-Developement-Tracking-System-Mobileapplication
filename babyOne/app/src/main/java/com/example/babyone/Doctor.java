17c0b3c057fd3d0de6b40721d8a3d2569a125946
dasuntheekshanagit <dasun.theekshana.git@gmail.com>
2023-07-02 09:44:48 +0530

    Add quarry to search the baby by name and store them in an array

--- babyOne/app/src/main/java/com/example/babyone/Doctor.java
+++ babyOne/app/src/main/java/com/example/babyone/Doctor.java
@@ -2,6 +2,8 @@ package com.example.babyone;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,11 +11,17 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.card.MaterialCardView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Doctor extends AppCompatActivity {
 
     private SearchView searchView;
     private MaterialCardView card1;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +31,42 @@ public class Doctor extends AppCompatActivity {
         searchView = findViewById(R.id.searchView);
         card1 = findViewById(R.id.card1);
 
+        db = FirebaseFirestore.getInstance();
+
         searchView.clearFocus();
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
 
                 card1.setVisibility(View.VISIBLE);
-                Toast.makeText(Doctor.this, "searching", Toast.LENGTH_SHORT).show();
+                Toast.makeText(Doctor.this, "searching " + query, Toast.LENGTH_SHORT).show();
+
                 //retrieve data from the database and display relevent data in the card view
+                db.collection("guardians")
+                        .whereGreaterThanOrEqualTo("babyname",query)
+                        .whereLessThan("babyname",query+"z")
+                        .get()
+                        .addOnCompleteListener(task ->{
+                           if (task.isSuccessful()){
+                               // Baby name, parentname,email and all details are store in hear
+                               List<Guardian> guardians = new ArrayList<>();
+                               System.out.println("Babyies");
+                               for (QueryDocumentSnapshot document : task.getResult()) {
+                                   // Extract the guardian data from the document
+                                   String babyName = document.getString("babyname");
+                                   String parentName = document.getString("parentname");
+                                   String email = document.getString("email");
+
+                                   // Print the baby names
+                                   System.out.println(babyName);
+
+                                   // Create a Guardian object
+                                   Guardian guardian = new Guardian(babyName, parentName, email);
+                                   guardians.add(guardian);
+                               }
+
+                           }
+                        });
                 return false;
             }
 
