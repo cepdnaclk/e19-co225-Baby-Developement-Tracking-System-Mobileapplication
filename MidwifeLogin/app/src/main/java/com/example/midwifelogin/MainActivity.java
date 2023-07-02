6c1ae6315ae2c2868c6731e3352b30540a93620e
RavinduBA <e19004@eng.pdn.ac.lk>
2023-07-02 15:58:39 +0530

    midwifelogin finished

--- MidwifeLogin/app/src/main/java/com/example/midwifelogin/MainActivity.java
+++ MidwifeLogin/app/src/main/java/com/example/midwifelogin/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.midwifelogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,9 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.ktx.FirebaseFirestoreKtxRegistrar;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +39,48 @@ public class MainActivity extends AppCompatActivity {
         Midwifename = (EditText) findViewById(R.id.editTextText);
         MidwifeReg =   (EditText) findViewById(R.id.editTextText2);
         enter = (Button) findViewById(R.id.button);
+
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String MidWifename =Midwifename.getText().toString();
+                String MidWifeReg =MidwifeReg.getText().toString();
+                Map<String,Object> user= new HashMap<>();
+                user.put("Docter Name",MidWifename);
+                user.put("Docter RegNo",MidWifeReg);
+
+                db.collection("MidWifeLog")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(MainActivity.this,"succesful",Toast.LENGTH_SHORT).show();
+
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+
+                                Toast.makeText(MainActivity.this,"failed",Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+
+
+
+            }
+        });
     }
+
+
+    public void updateText(View view){
+
+        System.out.println("Button clicked");
+
+    }
+
+
+
 
 }
