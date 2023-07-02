cc0f28e54febe1a62ad354f28df571b9102e10d1
RavinduBA <e19004@eng.pdn.ac.lk>
2023-07-02 13:08:23 +0530

    firebase connection made

--- MidwifeLogin/app/src/main/java/com/example/midwifelogin/MainActivity.java
+++ MidwifeLogin/app/src/main/java/com/example/midwifelogin/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.ktx.FirebaseFirestoreKtxRegistrar;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
@@ -15,19 +18,19 @@ public class MainActivity extends AppCompatActivity {
     EditText MidwifeReg;
     Button enter;
 
+   FirebaseFirestore db;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        db = FirebaseFirestore.getInstance();
         textView = (TextView) findViewById(R.id.textView);
         Midwifename = (EditText) findViewById(R.id.editTextText);
         MidwifeReg =   (EditText) findViewById(R.id.editTextText2);
         enter = (Button) findViewById(R.id.button);
     }
-    public void updateText(View view){
 
-        System.out.println("Button clicked");
-
-    }
 }
