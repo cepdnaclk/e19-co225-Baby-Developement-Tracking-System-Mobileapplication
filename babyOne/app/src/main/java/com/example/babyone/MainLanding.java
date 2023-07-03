1484da09508cdd8ce8d881c61bd17647ea9af313
dasuntheekshanagit <dasun.theekshana.git@gmail.com>
2023-07-03 13:56:05 +0530

    Resolve conflict on mainLanding and LoginActivity

--- babyOne/app/src/main/java/com/example/babyone/MainLanding.java
+++ babyOne/app/src/main/java/com/example/babyone/MainLanding.java
@@ -1,5 +1,10 @@
 package com.example.babyone;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
@@ -7,6 +12,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -20,12 +26,25 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainLanding extends AppCompatActivity {
 
     private ActivityMainLandingBinding binding;
-
+    private TextView txtvProfileName;
+    private TextView txtvProfileDesc;
+    private String email;
+    String babyTimestamp;
+    Period period;
+    String gender,age;
     @Override
     public void onBackPressed() {
         new AlertDialog.Builder(this)
@@ -56,17 +75,86 @@ public class MainLanding extends AppCompatActivity {
         binding = ActivityMainLandingBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        Fragment homeView = new homeFragment();
-        Fragment profileView = new profileFragment();
-        Fragment settingsView = new extrasFragment();
+        Bundle extras = getIntent().getExtras();
+        String sourceFragment = extras != null ? extras.getString("sourceFragment") : "";
+
+        //Create Fragments
+        homeFragment homeView ;
+        Fragment profileView;
+        Fragment extrasView;
+
         GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(binding.getRoot().getContext(), GoogleSignInOptions.DEFAULT_SIGN_IN);
-        FirebaseAuth firebaseAuth;
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
-        // Initialize firebase auth
-        firebaseAuth = FirebaseAuth.getInstance();
+        if (sourceFragment.equals("doctor")) {
+            extrasView = new MedicineFragment();
+            email = extras.getString("email");
+            NotificationService.sendNotification(this,"Wellcome Again Doctor, Lets track tour patients","Login");
+        } else if (sourceFragment.equals("midwife")) {
+            extrasView = new VaccineFragment();
+            email = extras.getString("email");
+            NotificationService.sendNotification(this,"Wellcome Again Midwife, Lets track tour patients","Login");
+        } else {
+            extrasView = new extrasFragment(); // Default fragment
 
-        if (savedInstanceState == null)
-            replaceFragment(homeView);
+            //Fetch baby deatils
+            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+            if (firebaseUser != null) {
+                email = firebaseUser.getEmail();
+            }
+            NotificationService.sendNotification(this,"Wellcome Again, Lets Track Your baby","Login");
+        }
+
+        //Binding elements
+        txtvProfileName = binding.txtvProfileName;
+        txtvProfileDesc = binding.txtvProfileDesc;
+
+        profileView = new profileFragment();
+
+        // Create db instance to fetch data
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        String collectionName = "guardians/";
+        //Overwrite onDataLoaded method
+        FirestoreHelper.readFromCollection(db, collectionName, email, new FirestoreHelper.FirestoreDataCallback() {
+            @Override
+            public void onDataLoaded(HashMap<String, Map<String, Object>> dataMap) {
+                // Handle the retrieved data here
+                for (Map.Entry<String, Map<String, Object>> entry : dataMap.entrySet()) {
+                    Map<String, Object> data = entry.getValue();
+                    for (Map.Entry<String, Object> fieldEntry : data.entrySet()) {
+                        String fieldName = fieldEntry.getKey();
+                        Object fieldValue = fieldEntry.getValue();
+                        if (fieldName.equals("babyname")){
+                            txtvProfileName.setText(fieldValue.toString());
+                        }
+                        if (fieldName.equals("baby_gender")){
+                            //txtvProfileDesc.setText(fieldValue.toString()+ ", 2 year old");
+                            gender = fieldValue.toString();
+                        }
+                        if (fieldName.equals("baby_bday")) {
+                            babyTimestamp = fieldValue.toString();
+                            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                            LocalDate babyBirthday = LocalDate.parse(babyTimestamp);
+                            LocalDate currentDate = LocalDate.now();
+                            period = Period.between(babyBirthday, currentDate);
+                            int years = period.getYears();
+                            int months = period.getMonths();
+                            age = years+" years " + months+" month old";
+                            //txtvProfileDesc.setText(years+"Y " + months+"M");
+                        }
+                    }
+                }
+                if (age != null && gender != null) {
+                    txtvProfileDesc.setText(gender + ", " + age);
+                }
+            }
+        });
+
+        homeView = new homeFragment();
+        homeView.setEmail(email);
+//        homeView.setWeight(50);
+//        homeView.setHeight(50);
+        replaceFragment(homeView);
 
 
         binding.bottomNavigationViewML.setOnItemSelectedListener(item -> {
@@ -80,7 +168,7 @@ public class MainLanding extends AppCompatActivity {
                     break;
 
                 case R.id.extras:
-                    replaceFragment(settingsView);
+                    replaceFragment(extrasView);
                     break;
             }
 
@@ -94,6 +182,8 @@ public class MainLanding extends AppCompatActivity {
                 if (task.isSuccessful()) {
                     // When task is successful, sign out from Firebase
                     firebaseAuth.signOut();
+                    // Clear cache
+                    clearCache();
                     // Display Toast
                     Toast.makeText(binding.getRoot().getContext(), "Logout successful", Toast.LENGTH_SHORT).show();
                     // Finish activity
@@ -104,12 +194,42 @@ public class MainLanding extends AppCompatActivity {
         }));
     }
 
-    private void replaceFragment(Fragment fragment) {
+    private void replaceFragment(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         // Add a fade-in animation
         fragmentTransaction.setCustomAnimations(android.R.anim.fade_in, 0, 0, 0);
         fragmentTransaction.replace(binding.frameLayoutML.getId(), fragment);
         fragmentTransaction.commit();
+    }
+
+    // Clear cache
+    private void clearCache() {
+        try {
+            File cacheDir = getCacheDir();
+            if (cacheDir != null && cacheDir.isDirectory()) {
+                deleteDir(cacheDir);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Delete directory recursively
+    private boolean deleteDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (String child : children) {
+                boolean success = deleteDir(new File(dir, child));
+                if (!success) {
+                    return false;
+                }
+            }
+            return dir.delete();
+        } else if (dir != null && dir.isFile()) {
+            return dir.delete();
+        } else {
+            return false;
+        }
     }
 }
